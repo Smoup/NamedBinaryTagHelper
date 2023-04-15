bb4dca9f9e8e4fcea13d94200378dea026f5dac1
Smoup <106016152+Smoup@users.noreply.github.com>
2023-04-15 17:21:15 +0300

    Initial commit

---
+++ src/main/java/main/nbth/NamedBinaryTagHelper.java
@@ -0,0 +1,17 @@
+package main.nbth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NamedBinaryTagHelper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
